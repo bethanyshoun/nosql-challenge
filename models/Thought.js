419b250564dd5a9734d9ae606f05a45abafc9e66
bethanyshoun <bethanyshoun@gmail.com>
2022-09-05 11:32:15 -0400

    Thought model and Reaction schema set up

--- models/Thought.js
+++ models/Thought.js
@@ -1,21 +1,78 @@
-// Thought
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-// thoughtText
-// String
-// Required
-// Must be between 1 and 280 characters
+//Reactions
+const ReactionSchema = new Schema(
+    {
+      // set custom id to avoid confusion with parent comment _id
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+      }
+    },
+    {
+      toJSON: {
+        getters: true
+      }
+    }
+  );
 
-// createdAt
-// Date
-// Set default value to the current timestamp
-// Use a getter method to format the timestamp on query
+//Thoughts
+const ThoughtSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    thoughtBody: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+      },
+    // reactions (These are like replies)
+    // Array of nested documents created with the reactionSchema
+    reactions: [ReactionSchema]
+},
+{
+    toJSON: {
+      getters: true
+    }
+}
+);
 
-// username (The user that created this thought)
-// String
-// Required
-
-// reactions (These are like replies)
-// Array of nested documents created with the reactionSchema
-
-// Schema Settings
 // Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
+
+
+
+
+
+
+
+
