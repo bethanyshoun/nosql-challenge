1f44dc4d74eb9fcf3837e10eda2e68a5e75d07d8
bethanyshoun <bethanyshoun@gmail.com>
2022-09-11 11:39:42 -0400

    udpate user-routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,7 +6,9 @@ const {
     getUserById,
     createUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    addFriend,
+    removeFriend,
 } = require('../../controllers/user-controller')
 
 
@@ -16,28 +18,30 @@ module.exports = router;
 // /api/users
 
 // GET all users
+router
+    .route('/')
+    .get(getAllUser)
+    .post(createUser);
 
 // GET a single user by its _id and populated thought and friend data
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
 
-// POST a new user:
+// Add and Delete Friends
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend);
+
+
+// BONUS: Remove a user's associated thoughts when deleted.
 
 // // example data
 // {
 //   "username": "lernantino",
 //   "email": "lernantino@gmail.com"
 // }
-// PUT to update a user by its _id
-
-// DELETE to remove user by its _id
-
-// BONUS: Remove a user's associated thoughts when deleted.
-
-
-//** DOES THIS GO HERE? **
-
-// /api/users/:userId/friends/:friendId
-
-// POST to add a new friend to a user's friend list
-
-// DELETE to remove a friend from a user's friend list
-
+module.exports = router;
